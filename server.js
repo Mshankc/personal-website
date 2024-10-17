071efa7f3afb8e086a9db7899d36220597ccb6e5
mshan <mshankc@gmail.com>
2024-10-17 12:34:23 +0530

    completed express

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-var express =require('express');
-var app =express();
+var express = require('express');
+var app = express();
 
-app.use(express.static('/public'));
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+app.use(express.static('public/css'));
 
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-
-app.get('/',function(req,res){
+// Render the 'index' view when visiting the root URL
+app.get('/', function(req, res) {
     res.render('index');
 });
+app.get('/add',function(req,res){
+    res.render('dbexample');
+});
 
-app.listen(8080);
+// Start the server on port 8080
+app.listen(8080, function() {
+    console.log('Server is running on port 8080');
+});
