92ba1383df4f58f2c74c892b9ada2f564366e29b
mshan <mshankc@gmail.com>
2024-10-17 12:08:48 +0530

    added node

---
+++ server.js
@@ -0,0 +1,14 @@
+var express =require('express');
+var app =express();
+
+app.use(express.static('/public'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.listen(8080);
